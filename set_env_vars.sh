b08679599f7bc4da4cad4fde724493517dbe11a0
Wille Marcel <wille@wille.blog.br>
2018-01-24 17:46:18 -0200

    switch to env vars

---
+++ set_env_vars.sh
@@ -0,0 +1,12 @@
+# DATABASE
+export IBGE_DB_HOST=localhost
+export IBGE_DB_NAME=ibge_import
+export IBGE_DB_USER=postgres
+export IBGE_DB_PASSWORD=postgres
+
+# PATH TO THE IBGE SHAPEFILES
+export IBGE_SHP_PATH=
+
+# MAPBOX ACCOUNT
+export MAPBOX_TOKEN=
+export MAPBOX_USERNAME=
